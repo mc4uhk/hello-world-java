910151b7855716599f5d49a43db5e184fafa5df0
mc4uhk <mc4u.hk@gmail.com>
2019-12-07 00:24:30 +0800

    commit from intellij

--- src/hk/mc4u/Main.java
+++ src/hk/mc4u/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello World !!");
         System.out.println("Hello World 2!!");
         System.out.println("Hello World 3!!");
+        System.out.println("Hello World 4!!");
     }
 }
